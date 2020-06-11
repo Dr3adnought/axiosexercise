8c5de8a918c891548e84642d2ea409ef383b4474
SailorJeff <jfrele14@gmail.com>
2020-06-10 23:04:53 -0400

    second commit, pokemon search

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
-document.addEventListener('DOMContentLoaded', function(){
-    axios.get('http://beer-me-python-backend.herokuapp.com/beers').then(function(result){
-        console.log(result.data)
-
-        // select beer image source
-        let beerImg = result.data[0].beer_label
-
-        // create img HTML element
-        let beer = document.createElement('img')
-
-        // add image source to img element
-        beer.src = beerImg
-        
-        // append image to document body
-        document.body.appendChild(beer)        
-    })
-
-})
+// wait till HTML loads then run this { } JavaScript
+document.addEventListener('DOMContentLoaded', function () {
+    let button = document.querySelector('#search-button');
+    let input = document.querySelector('#search-input');
+    let pokePic = document.querySelector('#pokePic');
+    let pokePic2 = document.querySelector('#pokePic2');
+    button.addEventListener('click', function () {
+      axios
+        .get('https://pokeapi.co/api/v2/pokemon/' + input.value)
+        .then(function (result) {
+          console.log(result.data.sprites.front_default);
+          pokePic.src = result.data.sprites.front_default;
+          pokePic2.src = result.data.sprites.back_default;
+        })
+        .catch(function(error){
+          console.log(error)
+          pokePic.src = 'https://vivianpetersoops.com/wp-content/uploads/2017/11/oops-orange.jpg'
+          pokePic2.src = ''
+        })
+    });
+  });
